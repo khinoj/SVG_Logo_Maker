51542a640113158bfe96e8ada92d63558dfc6ad3
khinoj <kristopher.hinojosa@gmail.com>
2023-09-03 12:46:10 -0400

    wrote classes with render and setup export of classes

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {Triangle, Square, Circle} = require('./lib/shapes');
+
+
 
 const questions = [
     {
         type: 'input',
-        title: 'characters',
-        message: 'Pick 3 letters',
+        name: 'characters',
+        message: 'Type 3 letters:',
         validate: function (input) {
-            if (input.lenth <= 3) {
-                return true
+            if (input.length <= '3') {
+                return true;
             } else {
                 return 'You must type 3 characters or less'
             }
@@ -16,17 +19,17 @@ const questions = [
     },
     {
         type: 'input',
-        title: 'textColor',
-        message: 'Pick a text color (OR a hexadecimal number)'
+        name: 'text_color',
+        message: 'Pick your text color (OR a hexadecimal number)'
     },
     {
         type: 'list',
-        title: 'shape',
-        message: ['Circle', 'Triangle', 'Square']
+        name: 'shape',
+        message: ['Triangle', 'Circle', 'Square']
     },
     {
         type: 'input',
-        title: 'shapeColor',
+        name: 'shape_color',
         message: 'Wat color do you want the shape (OR a hexdecimal number)'
     },
 ];
@@ -37,4 +40,8 @@ inquirer
     /* Pass your questions in here */
   .then((answers) => {
     // Use user feedback for... whatever!!
-  });
+    var characters = answers.characters.toUpperCase();
+    var text_color = answers.text_color;
+
+    }
+  );
